95bb0b4aadc515e18912453254d9b09f18d6fac5
richardparle <richardparle@gmail.com>
2022-03-29 19:52:18 +0100

    completed task 6

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { send } = require("express/lib/response");
 const app = express();
 const {
   getTopics,
   pathNotFoundErr,
   getArticleById,
-} = require("./controllers/news.controllers");
+  patchArticleById,
+  getUsers,
+} = require("./controllers/articles.controllers");
 
 app.use(express.json());
 
@@ -12,12 +15,23 @@ app.use(express.json());
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 
+// PATCH requests
+app.patch("/api/articles/:article_id", patchArticleById);
+
 // Error handling
-app.all("/*", pathNotFoundErr);
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Path not found" });
+});
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02") res.status(400).send({ msg: "Bad request" });
   next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  }
 });
 
 app.use((err, req, res, next) => {
---
+++ controllers/articles.controllers.js
@@ -0,0 +1,36 @@
+const {
+  fetchTopics,
+  fetchArticleById,
+  updateArticleById,
+} = require("../models/articles.models");
+
+exports.getTopics = (req, res, next) => {
+  fetchTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => next(err));
+};
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  fetchArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+};
+
+exports.patchArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+
+  updateArticleById(article_id, inc_votes)
+    .then((data) => {
+      res.status(200).send({ msg: data });
+    })
+    .catch((err) => next(err));
+};
