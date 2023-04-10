411906d0422225a46fc7b42194605e1b83de6b09
Richard <richardparle@gmail.com>
2023-04-10 20:07:28 +0100

    updated the connection file allow connection to elephantSQL

--- db/connection.js
+++ db/connection.js
@@ -3,19 +3,16 @@ const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
+  path: `${__dirname}/../.env.development`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config =
-  ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        max: 2,
-      }
-    : {};
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  max: 2,
+};
 
 module.exports = new Pool(config);
